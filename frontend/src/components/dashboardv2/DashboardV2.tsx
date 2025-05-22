e054f094226802bb74baeef5aae4429c28af1d24
pablis77 <pablomgallegos@gmail.com>
2025-05-22 23:09:08 +0200

    fase 1 de anular el dashbaordoriginal por el v2... seguimos

--- frontend/src/components/dashboardv2/DashboardV2.tsx
+++ frontend/src/components/dashboardv2/DashboardV2.tsx
@@ -186,16 +186,7 @@ const DashboardV2: React.FC = () => {
       className={`dashboard-container ${darkMode ? 'theme-dark' : 'theme-light'}`}
       data-component-name="DashboardV2"
     >
-      {/* [DASHBOARDV2] Enlace para volver al Dashboard original */}
-      <a 
-        href="/dashboard" 
-        className="fixed top-4 right-4 bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full shadow-lg z-50 flex items-center gap-2"
-      >
-        <span>Volver al Dashboard</span>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L4.414 9H17a1 1 0 110 2H4.414l5.293 5.293a1 1 0 010 1.414z" clipRule="evenodd" />
-        </svg>
-      </a>
+      {/* Botón para volver al Dashboard original eliminado - Ya no es necesario */}
       
       {/* [DASHBOARDV2] Botón para cambiar tema */}
       <button 
