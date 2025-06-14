df2ff6e2483b3db2f3466b263c29c99ad58050a3
pablis77 <pablomgallegos@gmail.com>
2025-06-14 13:04:40 +0200

    Actualizar .gitignore para excluir archivos grandes (>100MB)

--- AWS_AMPLIFY/varios previos/output-sources-web/run-2025-06-14T10-18-59.967Z/js/localhost_3000_src_components_LanguageSwitcher_astro_astro_type_style_index_0_lang_css.js
+++
@@ -1,6 +0,0 @@
-import { createHotContext as __vite__createHotContext } from "/@vite/client";import.meta.hot = __vite__createHotContext("/src/components/LanguageSwitcher.astro?astro&type=style&index=0&lang.css");import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from "/@vite/client"
-const __vite__id = "C:/Proyectos/claude/masclet-imperi-web/frontend/src/components/LanguageSwitcher.astro?astro&type=style&index=0&lang.css"
-const __vite__css = ".language-switcher[data-astro-cid-a2mxz4y6]{display:inline-block;margin-left:1rem}"
-__vite__updateStyle(__vite__id, __vite__css)
-import.meta.hot.accept()
-import.meta.hot.prune(() => __vite__removeStyle(__vite__id))
--- AWS_AMPLIFY/varios previos/output-sources-web/run-2025-06-14T10-18-59.967Z/js/localhost_3000_src_components_layout_Sidebar_astro_astro_type_style_index_0_lang_css.js
+++
@@ -1,6 +0,0 @@
-import { createHotContext as __vite__createHotContext } from "/@vite/client";import.meta.hot = __vite__createHotContext("/src/components/layout/Sidebar.astro?astro&type=style&index=0&lang.css");import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from "/@vite/client"
-const __vite__id = "C:/Proyectos/claude/masclet-imperi-web/frontend/src/components/layout/Sidebar.astro?astro&type=style&index=0&lang.css"
-const __vite__css = ".masclet-sidebar[data-astro-cid-k4cmclh2]{transition:transform 0.3s ease-in-out}@media (max-width: 768px){.masclet-sidebar[data-astro-cid-k4cmclh2]{position:fixed;left:0;top:0;width:80%;max-width:280px}}"
-__vite__updateStyle(__vite__id, __vite__css)
-import.meta.hot.accept()
-import.meta.hot.prune(() => __vite__removeStyle(__vite__id))
